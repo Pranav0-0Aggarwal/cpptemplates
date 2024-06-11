dd1d2eba1874f7e23215bceee6d9b7a5c5fa403f
Pranav0-0Aggarwal <96537779+Pranav0-0Aggarwal@users.noreply.github.com>
2024-06-11 18:22:20 +0530

    Create boilerplate.cpp

---
+++ boilerplate.cpp
@@ -0,0 +1,31 @@
+//Author: Sexy_Penguin
+
+#include<bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "debug/debug.cpp"
+#else
+#define debug(...)
+#endif
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define pb push_back
+#define int long long
+#define endl '\n'
+#define yes{cout<<"YES"<<endl;return;}
+#define no{cout<<"NO"<<endl;return;}
+const int MOD=1000000007;
+/************************SOLUTION***************************/
+
+
+void solve(){
+    
+}
+
+
+int32_t main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+}
